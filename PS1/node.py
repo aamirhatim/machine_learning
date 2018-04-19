241c4f6e768cad86cb754e3d2ccac1769ce7e153
Aamir Husain <aamir300@gmail.com>
2018-04-18 23:46:47 -0500

    Remove visitid attribute

--- PS1/node.py
+++ PS1/node.py
@@ -3,5 +3,3 @@ class Node:
         self.label = None                   # Name of attribute the node is split on
         self.children = {}                  # Dictionary of child branches and the data sets inside them
         self.mode = None                    # Mode class of all samples in node
-        self.visited = 0                    # Flag to indicate this node has been visited (for pruning)
-        # you may want to add additional fields here...
