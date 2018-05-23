11b1e7f966b548fd10fb7756535d6af2f337077f
Aamir Husain <aamir300@gmail.com>
2018-05-23 10:51:13 -0500

    Testing the grader

--- PS1/test.py
+++ PS1/test.py
@@ -2,17 +2,20 @@
 from parse import parse
 import ID3
 import unit_tests as tests
+import grader
 
 def main():
-    e = parse("house_votes_84.data")
-    training, validation = ID3.split(e,50)
+    # e = parse("house_votes_84.data")
+    # training, validation = ID3.split(e,50)
+    #
+    # f = ID3.ID3(training, 'empty')
+    # # print ID3.test(f, training)
+    # # print ID3.test(f, validation)
+    # ID3.prune(f, validation)
+    #
+    # tests.testPruningOnHouseData("house_votes_84.data")
+    grader.testID3AndEvaluate()
 
-    f = ID3.ID3(training, 'empty')
-    # print ID3.test(f, training)
-    # print ID3.test(f, validation)
-    ID3.prune(f, validation)
-
-    tests.testPruningOnHouseData("house_votes_84.data")
 
 if __name__ == "__main__":
     main()
