fc1fa6082f5942d1bdd0d69dc6779e22bc61f3de
Aamir Husain <aamir300@gmail.com>
2018-05-25 00:53:45 -0500

    Complete fnn code, add Tensorflow images

--- PS3/fnn.py
+++ PS3/fnn.py
@@ -231,27 +231,27 @@ class Layer(object):
         #     A matrix of shape (N, H)
         #     N is the number of data points.
         #     H is the number of hidden units in this layer.
-        d_scores = np.zeros_like(self.a)
+        d_scores = np.multiply(d_outputs, self.d_activate(self.a))
 
         # self.d_b:
         #     Derivatives of the loss w.r.t the bias, averaged over all data points.
         #     A matrix of shape (1, H)
         #     H is the number of hidden units in this layer.
-        self.d_b = np.zeros_like(self.b)
+        self.d_b = np.dot(np.ones((1, d_scores.shape[0])), d_scores)
 
         # self.d_w:
         #     Derivatives of the loss w.r.t the weight matrix, averaged over all data points.
         #     A matrix of shape (H_-1, H)
         #     H_-1 is the number of hidden units in previous layer
         #     H is the number of hidden units in this layer.
-        self.d_w = np.zeros_like(self.w)
+        self.d_w = np.dot(self.inputs.T, d_scores)
 
         # d_inputs:
         #     Derivatives of the loss w.r.t the previous layer's activations/outputs.
         #     A matrix of shape (N, H_-1)
         #     N is the number of data points.
         #     H_-1 is the number of hidden units in the previous layer.
-        d_inputs = np.zeros([d_scores.shape[0], self.w.shape[0]])
+        d_inputs = np.dot(d_scores, self.w.T)
 
         # End of the code to modify
         ###################################
