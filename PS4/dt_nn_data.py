321662deaa1a860afd48578d6cfe8de23c8752e2
Aamir Husain <aamir300@gmail.com>
2018-06-07 19:41:10 -0500

    Generate data for testing

--- PS4/dt_nn_data.py
+++ PS4/dt_nn_data.py
@@ -3,6 +3,24 @@ import pandas as pd
 from random import randint
 
 num_att = 10
-num_samples = 1000
+num_samples = 20
 
-print randint(0,1)
+data = []
+for i in range(num_samples):
+    empty = []
+    data.append(empty)
+
+for i in range(num_samples):                # Populate all but last attribute with binary values
+    for j in range(num_att-1):
+        data[i].append(randint(0,1))
+
+count = 0
+while count < num_samples/2:                # Classify half of data as 1 and the other half as 0
+    data[count].append(1)
+    count += 1
+while count < num_samples:
+    data[count].append(0)
+    count += 1
+
+for i in data:
+    print i
