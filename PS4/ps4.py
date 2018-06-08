26dc1cc8ea3e165b5d408d9e4d79670d6694c24f
Aamir Husain <aamir300@gmail.com>
2018-06-08 00:52:06 -0500

    Implement for loop to cycle through all test images

--- PS4/ps4.py
+++ PS4/ps4.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2.7
 import numpy as np
+import json
 
 def normalize(matrix):
     squared = matrix**2                         # Square elements
@@ -38,9 +39,63 @@ def img_compare():
     print 'mj1-cat:',cosine_sim(mj1_vgg, cat_vgg)
     print 'mj2-cat:',cosine_sim(mj2_vgg, cat_vgg)
 
+def find_neighbor(test_name, train, images, captions, pixel, vgg):
+    # test_name = test[0]                                   # Start by finding similarity to first image in training set
+    test_ind = images.index(test_name)                      # Get index of test image in the images set
+    train_ind = images.index(train[0])                      # Get index of training image in the images set
+
+    pixel_train = pixel[train_ind]                          # Get pixel representation of training image
+    pixel_test = pixel[test_ind]                            # Get pixel representation of test image
+    sim = cosine_sim(pixel_test, pixel_train)               # Compute baseline similarity for pixel rep
+    best_pixel = {'name':train[0], 'similarity':sim}        # Initialize best pixel value for comparison with rest of the training set
+
+    vgg_train = vgg[train_ind]                              # Get vgg representation of training image
+    vgg_test = vgg[test_ind]                                # Get vgg representation of test image
+    sim = cosine_sim(vgg_test, vgg_train)                   # Compute baseline similarity for vgg rep
+    best_vgg = {'name':train[0], 'similarity':sim}          # Initialize best vgg value for comparison with rest of the training set
+
+    for i in range(len(train)-1):                           # Cycle through remainder of training set
+        train_ind = images.index(train[i+1])                # Get index
+
+        pixel_train = pixel[train_ind]                      # Get pixel representations
+        pixel_test = pixel[test_ind]
+        sim = cosine_sim(pixel_test, pixel_train)           # Compute similarity for pixel rep
+        if sim > best_pixel['similarity']:                  # Check if similarity is better than current 'best' value
+            best_pixel['name'] = train[i+1]
+            best_pixel['similarity'] = sim
+
+        vgg_train = vgg[train_ind]                          # Get vgg representations
+        vgg_test = vgg[test_ind]
+        sim = cosine_sim(vgg_test, vgg_train)               # Compute similarity for vgg rep
+        if sim > best_vgg['similarity']:                    # Check if similarity is better than current 'best' value
+            best_vgg['name'] = train[i+1]
+            best_vgg['similarity'] = sim
+
+    return best_pixel, best_vgg
+
 def captions():
     with open('dataset.json') as f:
         data = json.load(f)
+    train = data['train']                                   # File names of training images
+    test = data['test']                                     # File names of test images
+    images = data['images']                                 # File names of all images
+    captions = data['captions']                             # Captions for each image
 
-    pixel = np.load('pixel_rep.npy')
+    pixel = np.load('pixel_rep.npy')                        # Load pixel and vgg representations of images
     vgg = np.load('vgg_rep.npy')
+
+    for i in test:
+        # Find nearest neighbor uing cosine similarity
+        neighbor_pixel, neighbor_vgg = find_neighbor(i, train, images, captions, pixel, vgg)
+
+        # Find caption of neighbor
+        caption_predict = captions[neighbor_pixel['name']]
+        print 'PREDICTED CAPTION:',caption_predict
+
+        # Find actual caption
+        caption_actual = captions[i]
+        # print 'ACTUAL CAPTION:',caption_actual
+
+
+
+captions()
